fb4f16b2e23929484fa2797017dce1a02c645264
David Bador <david.bador@gmail.com>
2020-04-23 22:28:18 +0300

    completed exercise 2 from day 3

--- user.py
+++ user.py
@@ -6,8 +6,8 @@ class User:
     __authorize_user = []
 
     def __init__(self):
-        file_handle = FileHandler('user.csv')
-        self.__users = file_handle.get_data()
+        self.file_handle = FileHandler('user.csv')
+        self.__users = self.file_handle.get_data()
 
     def user_auth(self, name, password):
         name = name.split()
@@ -19,3 +19,19 @@ class User:
         except Exception as e:
             print(e)
             raise
+
+    def add_user(self, id, **kwargs):
+        for row in self.__users:
+            if row['user_id'] == id:
+                return False
+
+        answer = self.file_handle.append_to_csv('user.csv', kwargs)
+        if answer:
+            return True
+        else:
+            return False
+
+
+user = User()
+print(user.add_user('1200000012', user_id='1200000012', first='Sacha', last='Rebbouh', password='sachsach111',
+                    position='Rabbi', salary='200000', role='user'))
